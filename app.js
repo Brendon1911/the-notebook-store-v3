92377d4eac6ca39df9528f1eac6e58bcd528b2fa
Brendon Richards <brendonrichards93@gmail.com>
2018-09-12 13:06:31 +0000

    Setup express-session, passport and connect-flash in app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-// Requirements
+// Node modules
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dbConfig = require('./config/db.config');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+
 
 // Require routes
 const indexRoute = require("./api/routes/indexRoute");
@@ -14,7 +18,6 @@ const app = express();
 
 // Parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
 // Parse requests of content-type - application/json
 app.use(bodyParser.json());
 
@@ -28,31 +31,33 @@ mongoose.connect(dbConfig.url, {
   process.exit();
 });
 
+// Use express-session
+app.use(session({ secret: 'mySuperSecretSecret', resave: false, saveUninitialized: false }));
+// Use connect-flash
+app.use(flash());
+// Use passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Use client files
 app.use(express.static(path.join(__dirname, '/client')));
-
 // Use custom JavaScript
 app.use('/client/js', express.static(path.join(__dirname, '/client/js')));
-
 // Use Semantic UI CSS
 app.use('/client/css', express.static(path.join(__dirname, '/client/libs/semantic/dist/')));
-
 //Use Semantic UI JS
 app.use('/client/js', express.static(path.join(__dirname, '/client/libs/semantic/dist/')));
-
 // Use jQuery
 app.use('/client/js/libs', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 
 //Set views
 app.set('views', './client/views');
-
 // Set view engine
 app.set('view engine', 'ejs');
 
+// Routes middleware
 app.use('/', indexRoute);
-
 app.use('/notebooks', notebookRoutes);
-
 app.use('/signup', userRoutes);
 
 module.exports = app;
